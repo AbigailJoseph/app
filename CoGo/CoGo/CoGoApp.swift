069eed9c1a6c8645249862221aae7a5688632743
AbigailJoseph <60202624+AbigailJoseph@users.noreply.github.com>
2022-01-06 15:48:13 -0500

    Add files via upload

---
+++ CoGo/CoGo/CoGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoGoApp.swift
+//  CoGo
+//
+//  Created by Abigail Joseph on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CoGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
